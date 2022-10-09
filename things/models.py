8afe9525235c38f1900ed148e85d0318533197a3
muzami1ali <muzamil.ali@kcl.ac.uk>
2022-10-09 14:31:54 +0100

    Rename thing from things.models to Thing

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
-class thing(models.Model):
+class Thing(models.Model):
     name= models.CharField(max_length=100)
     description= models.TextField()
     quantity= models.IntegerField()
