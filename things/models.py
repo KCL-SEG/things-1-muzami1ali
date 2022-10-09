ef6020ed493c241133d1d559d8077f15e840a013
muzami1ali <muzamil.ali@kcl.ac.uk>
2022-10-09 14:27:39 +0100

    Add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class thing(models.Model):
+    name= models.CharField(max_length=100)
+    description= models.TextField()
+    quantity= models.IntegerField()
