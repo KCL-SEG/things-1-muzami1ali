e919ceb7341a34884cab7d77646efea04b428fda
muzami1ali <muzamilalibosan@icloud.com>
2022-10-27 14:44:43 +0100

    Correct validator usage

--- things/models.py
+++ things/models.py
@@ -1,17 +1,16 @@
 # from unittes...t.ut......il import _MAX_LENGTH
-from django.core.validators import MaxLengthValidator,MinLengthValidator
+from django.core.validators import MaxValueValidator,MinValueValidator, MaxLengthValidator
 from django.db import models
-# from django.db.models.fields import unique
-from django.db.models import fields
+# from django.db.models.Field import validators
+# from django.db.models import fields
 
 
 class Thing(models.Model):
     name= models.CharField(max_length=30, unique=True, blank=False)
-    description= models.TextField(max_length=120, blank=True)
-    quantity= models.IntegerField(
+    description= models.TextField(blank=True, validators= [MaxLengthValidator(120)])
+    quantity= models.IntegerField( 
         validators=[
-            MaxLengthValidator(limit_value=100,message='Quantity cannot be greater than 100'),
-            MinLengthValidator(limit_value=0, message='Quantity cannot be less than 0')
+            MaxValueValidator(100),
+            MinValueValidator(0),
         ]
-          
     )
